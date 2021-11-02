f54dd5d2a8462138078eb05af177d18f9bbd5332
Kan-E <77435195+Kan-E@users.noreply.github.com>
2021-11-02 14:08:52 +0900

    bug fix

---
+++ R/example dataset.R
@@ -0,0 +1,19 @@
+#' Row_count_3conditions
+#'
+#' @description example data for 3 conditions comparison analysis
+#' @docType data
+#' @name Row_count_3conditions
+#' @usage data(Row_count_3conditions)
+#' @format A data frame with 26334 rows and 6 variables
+NULL
+
+
+#' Row_count_data
+#'
+#' @description example data for pairwise comparison analysis
+#' @docType data
+#' @name Row_count_data
+#' @usage data(Row_count_data)
+#' @format A data frame with 26334 rows and 4 variables
+NULL
+
